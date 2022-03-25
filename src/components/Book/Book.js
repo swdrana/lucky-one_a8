d681126cd0ab9d5a3fd609220b14164104ba3253
swdrana <tctr2s@gmail.com>
2022-03-26 01:10:26 +0600

    book ui changed

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -6,14 +6,14 @@ const Book = (props) => {
   return (
     <div className="single-book">
       <img src={book.img} alt="" />
-        <h3>{book.name}</h3>
       <div className="info">
+        <h3>{book.name}</h3>
         <p>প্রকাশকঃ {book.publications}</p>
         <p>বিভাগঃ {book.category}</p>
         <p><strong>মূল্যঃ {book.price} টাকা</strong></p>
         <p>রেটিংঃ {book.ratings}</p>
-      </div>
       <button>Add to cart</button>
+      </div>
     </div>
   );
 };
