c778548e2166fd74c5735883956dc07a0c9415c9
swdrana <tctr2s@gmail.com>
2022-03-25 23:46:49 +0600

    header section work finish

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            {/* <img src="https://img.favpng.com/19/10/15/logo-buenos-aires-international-book-fair-wikibooks-image-png-favpng-imtBb21Mkqm9jPq81QdsLx2Xq.jpg" alt="" /> */}
+            <h1>Book Fair</h1>
+        </div>
+    );
+};
+
+export default Header;
