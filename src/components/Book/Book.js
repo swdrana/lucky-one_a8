b7b600e49e656087deffae2b1b4a0b3776b560e5
swdrana <tctr2s@gmail.com>
2022-03-26 13:33:43 +0600

    book section work finish

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,19 +1,28 @@
 import React from "react";
 import "./Book.css";
 const Book = (props) => {
-  const { book,addToCart } = props;
+  //destructuring object
+  const { book, addToCart } = props;
   // console.log(addToCart);
+
   return (
     <div className="single-book">
       <img src={book.img} alt="" />
+
+      {/* show book info from api / json */}
       <div className="info">
         <h3>{book.name}</h3>
         <p>প্রকাশকঃ {book.publications}</p>
         <p>বিভাগঃ {book.category}</p>
-        <p><strong>মূল্যঃ {book.price} টাকা</strong></p>
+        <p>
+          <strong>মূল্যঃ {book.price} টাকা</strong>
+        </p>
         <p>রেটিংঃ {book.ratings}</p>
-      <button onClick={()=>addToCart(book)}>Add to cart {`  `}
-      <i className="fa fa-cart-plus"></i></button>
+        {/* button with font awesome icon */}
+        <button onClick={() => addToCart(book)}>
+          Add to cart {`  `}
+          <i className="fa fa-cart-plus"></i>
+        </button>
       </div>
     </div>
   );
